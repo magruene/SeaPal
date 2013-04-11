c2632ecf054032d2b1dfbcd4f88fabf3ae41ba12
Marc Grünewald <marc_gruenewald@gmx.de>
2013-04-11 11:04:56 +0200

    PHP change

--- SeaPal/WebContent/SeaPalWeatherForm.php
+++ SeaPal/WebContent/SeaPalWeatherForm.php
@@ -1,27 +1,6 @@
 <!DOCTYPE html>
 <html lang="en" class="fuelux">
-<head>
-	<link href="http://exacttarget.github.com/fuelux/vendor/fuelux/css/fuelux.css" rel="stylesheet" />
-<link href="http://fuelcdn.com/fuelux-imh/2.2/css/fuelux-responsive.css" rel="stylesheet" />
-<link href="css/bootstrap.css" rel="stylesheet" type="text/css" />
-<link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
-<link href="css/bootstrap-responsive.min.css" rel="stylesheet">
-<link rel="stylesheet" href="http://code.jquery.com/ui/1.10.2/themes/smoothness/jquery-ui.css" />
-<link rel="stylesheet" href="css/timepicker.css" />
-<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js" type="text/javascript"></script>
-<script src="http://fuelcdn.com/fuelux-imh/2.2/loader.min.js" type="text/javascript"></script>
-<script type="text/javascript" src="http://code.jquery.com/jquery.min.js"></script>
-<script type="text/javascript" src="http://twitter.github.com/bootstrap/1.4.0/bootstrap-tabs.js"></script>
-<script src="http://code.jquery.com/jquery-1.9.1.js"></script>
-<script src="http://code.jquery.com/ui/1.10.2/jquery-ui.js"></script>
-<script src="http://netdna.bootstrapcdn.com/twitter-bootstrap/2.3.0/js/bootstrap.min.js"></script>
-<script src="js/jquery-ui-timepicker-addon.js"></script>
-<script>
-	$(function() {
-		$('#datepicker').datetimepicker();
-	});
-</script>
-</head>
+<?php include("header.php")?>
 <body>
 <div class="row-fluid">
 <div class="span6 offset3 ">
@@ -178,9 +157,15 @@ Saved changes successfully!
 		</div>
 	</div>
 </div>
-
 </form>
 </div>
 </div>
 </body>
 </html>
+
+<?php
+$hit_count = @file_get_contents('count.txt'); 
+echo $hit_count; 
+$hit_count++; 
+@file_put_contents('count.txt', $hit_count); 
+?>
