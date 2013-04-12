aa7641910411ddad728f5f843a8e27dde638f48a
PiEt <PiEt@hugonb2>
2013-04-12 11:46:00 +0200

    Attribut WaveDirection hinzugefügt

--- SeaPal/WebContent/mysql.php
+++ SeaPal/WebContent/mysql.php
@@ -1,5 +1,5 @@
 <?php
-$con	=	mysql_connect("localhost","root","root");
+$con	=	mysql_connect("localhost","testuser","testuser");
 
 $db_selected = mysql_select_db('webtech2013', $con);
 if (!$db_selected) {
@@ -12,8 +12,8 @@ if (!$db_selected) {
 	}
 	//	Create	table
 	mysql_select_db("webtech2013",	$con);
-	$sql	=	"CREATE	TABLE	WeatherData
-			(ID int not null auto_increment,
+	$sql	=	"CREATE	TABLE	WeatherData (
+			ID int not null auto_increment,
 			WindStrength varchar(15),
 			WindDirection varchar(15),
 			AirPressure varchar(15),
@@ -22,8 +22,10 @@ if (!$db_selected) {
 			Clouds varchar(1),
 			Rain varchar(1),
 			WaveHeight varchar(15),
+			WindDirection varchar(10),
 			DateTime datetime,
-			Primary Key(ID))";
+			Primary Key(ID)
+			)";
 	//	Execute	query
 	mysql_query($sql,$con);
 }
